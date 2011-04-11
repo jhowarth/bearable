1f5ea6d31d80a10f4b06209b52fa8a5edbe7de9b
Sikul <jahowarth@gmail.com>
2011-04-10 20:36:35 -0400

    Edited tests to include test for content.

--- test/welcome/test/schema.clj
+++ test/welcome/test/schema.clj
@@ -3,21 +3,21 @@
   (:use [clojure.test]))
 
 (deftest ordered-list
-  (is (= "<xs:element name=\"ordered-list\"><xs:complexType><xs:sequence></xs:sequence></xs:complexType></xs:element>"
-         (schema [:ordered-list! {:name "ordered-list"}] "xs"))))
+  (is (= "<xs:element name=\"ordered-list\"><xs:complexType><xs:sequence><content/></xs:sequence></xs:complexType></xs:element>"
+         (schema [:ordered-list! {:name "ordered-list"} [:content]] "xs"))))
 
 (deftest unordered-list
-  (is (= "<xs:element name=\"unordered-list\"><xs:complexType><xs:all></xs:all></xs:complexType></xs:element>"
-         (schema [:unordered-list! {:name "unordered-list"}] "xs"))))
+  (is (= "<xs:element name=\"unordered-list\"><xs:complexType><xs:all><content/></xs:all></xs:complexType></xs:element>"
+         (schema [:unordered-list! {:name "unordered-list"} [:content]] "xs"))))
 
 (deftest choice-list
-  (is (= "<xs:element name=\"choice\"><xs:complexType><xs:choice></xs:choice></xs:complexType></xs:element>"
-         (schema [:choice! {:name "choice"}] "xs"))))
+  (is (= "<xs:element name=\"choice\"><xs:complexType><xs:choice><content/></xs:choice></xs:complexType></xs:element>"
+         (schema [:choice! {:name "choice"} [:content]] "xs"))))
 
 (deftest restricted-simple
-  (is (= "<xs:element name=\"restricted\"><xs:simpleType><xs:restriction base=\"xs:string\"></xs:restriction></xs:simpleType></xs:element>"
-         (schema [:restricted! {:name "restricted" :base "xs:string"}] "xs"))))
+  (is (= "<xs:element name=\"restricted\"><xs:simpleType><xs:restriction base=\"xs:string\"><content/></xs:restriction></xs:simpleType></xs:element>"
+         (schema [:restricted! {:name "restricted" :base "xs:string"} [:content]] "xs"))))
 
 (deftest restricted-complex
-  (is (= "<xs:element name=\"restricted\"><xs:complexType><xs:restriction base=\"xs:string\"></xs:restriction></xs:complexType></xs:element>"
-         (schema [:restricted! {:name "restricted" :base "xs:string" :type "xs:complexType"}] "xs"))))
+  (is (= "<xs:element name=\"restricted\"><xs:complexType><xs:restriction base=\"xs:string\"><content/></xs:restriction></xs:complexType></xs:element>"
+         (schema [:restricted! {:name "restricted" :base "xs:string" :type "xs:complexType"} [:content]] "xs"))))
